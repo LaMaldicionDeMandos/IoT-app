1590f332037aceba3502a33e20f43e88386727de
mapsut pasut <mpasut@riu.com>
2024-04-25 13:21:42 -0300

    trabajo en el formulario de login

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iot_app/components/PasswordTextField.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +13,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'IoT Project',
       theme: ThemeData(
+        textSelectionTheme: const TextSelectionThemeData(
+          cursorColor: Colors.pink
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          fillColor: const Color(0xfff1f1f1),
+
+          contentPadding: const EdgeInsets.symmetric(horizontal: 20),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(24),
+              borderSide: BorderSide.none
+
+          )
+        ),
+        fontFamily: 'Lato',
          colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
@@ -30,11 +46,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String _username = '';
 
-  void _incrementCounter() {
+  void _setUsername() {
     setState(() {
-      _counter++;
     });
   }
 
@@ -45,7 +60,19 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(widget.title, style: const TextStyle(fontSize: 42, fontWeight: FontWeight.bold)),
+            Padding(padding: EdgeInsets.symmetric(horizontal: 32, vertical: 56),
+              child: Text(widget.title, style: const TextStyle(fontSize: 36, fontWeight: FontWeight.bold)),
+            ),
+            const Padding(padding: EdgeInsets.symmetric(horizontal: 32, vertical: 8),
+              child: TextField(
+                keyboardType: TextInputType.emailAddress,
+                style: TextStyle(fontFamily: 'Roboto'),
+                decoration: InputDecoration(hintText: 'Email')
+              )
+            ),
+            const Padding(padding: EdgeInsets.symmetric(horizontal: 32, vertical: 8),
+                child: PasswordTextField()
+            )
           ],
         ),
       ),
